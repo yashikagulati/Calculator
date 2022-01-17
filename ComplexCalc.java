1ad6d46bf98b0ba13a8b53947d0b265d7b6e4db7
yashikagulati <40389777+yashikagulati@users.noreply.github.com>
2022-01-17 21:27:57 +0530

    Add files via upload

---
+++ ComplexCalc.java
@@ -0,0 +1,73 @@
+package com.example.java;
+
+import java.util.Scanner;
+public class ComplexCalc {
+
+	public static void main(String[] args) {
+		
+		String s1 = getInput("Enter a numeric value: ");
+		String s2 = getInput("Enter a numeric value: ");
+		String ch = getInput("Choose an operation: + - * / ");
+		
+		double result = 0;
+		
+		try {
+			switch(ch) {
+			case "+":
+				result = addValues(s1,s2);
+				break;
+			case "-":
+				result = subtractValues(s1,s2);
+				break;
+			case "*":
+				result = multiplyValues(s1,s2);
+				break;
+			case "/":
+				result = divideValues(s1,s2);
+				break;
+			default:
+				System.out.println("Unrecognized operation");;
+				return;
+					
+			}
+			
+			System.out.println("The answer is: " + result);
+		}
+		catch(Exception e)
+		{
+			System.out.println("Number Formatting exception " + e.getMessage());
+		}
+	}
+	
+	private static double addValues(String s1, String s2) {
+		double d1 = Double.parseDouble(s1);
+		double d2 = Double.parseDouble(s2);
+		return d1 + d2;
+	}
+	
+	private static double subtractValues(String s1, String s2) {
+		double d1 = Double.parseDouble(s1);
+		double d2 = Double.parseDouble(s2);
+		return d1 - d2;
+	}
+	
+	private static double multiplyValues(String s1, String s2) {
+		double d1 = Double.parseDouble(s1);
+		double d2 = Double.parseDouble(s2);
+		return d1 * d2;
+	}
+	
+	private static double divideValues(String s1, String s2) {
+		double d1 = Double.parseDouble(s1);
+		double d2 = Double.parseDouble(s2);
+		return d1 / d2;
+	}
+	
+	private static String getInput(String prompt)
+	{
+		System.out.print(prompt);
+		Scanner sc = new Scanner(System.in);
+		return sc.nextLine();
+	}
+
+}
